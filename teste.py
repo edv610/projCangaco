f94129767f943b67e4ea4e47fb0ed3df8cdb9d93
victor.lins <victor.lins@orubetecnologia.com.br>
2020-11-21 19:53:23 -0300

    Adicionando classe produto

--- teste.py
+++ teste.py
@@ -5,91 +5,87 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 db = SQLAlchemy(app)
-#----------------------------------------------------------------CLASSE USUÁIO--------------------------------------------------------------------
-class Pessoa(db.Model):
+#----------------------------------------------------------------CLASSE PRODUTO--------------------------------------------------------------------
+class Produto(db.Model):
     #CRIAÇÃO DA TABELA DO BANCO
-    __tablename='cliente'
-    _id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nome = db.Column(db.String)
-    telefone = db.Column(db.String)
-    cpf = db.Column(db.String)
-    email = db.Column(db.String)
+    __tablename='produto'
+    _codProduto = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nomeProduto = db.Column(db.String)
+    valorProduto = db.Column(db.Float)
+    qntEstoque = db.Column(db.Integer)
 
     #CONSTRUTOR
-    def __init__(self,nome,telefone,cpf,email):
-        self.nome = nome
-        self.telefone = telefone
-        self.cpf = cpf
-        self.email = email
+    def _init_(self,nomeProduto,valorProduto,qntEstoque):
+        self.nomeProduto = nomeProduto
+        self.valorProduto = valorProduto
+        self.qntEstoque = qntEstoque
 
 db.create_all()
 
-#ROTA DA PAGINA INDEX
 @app.route('/')
 def index():
     return render_template('index.html')
 
 #ROTA DA  PAGINA DO CADASTRO
-@app.route('/cadastrar')
-def cadastrar():
-    return render_template('cadastro.html')
+@app.route('/produto/cadastrar')
+def cadastrarProdutos():
+    return render_template('cadastroProdutos.html')
 
 #ROTA DO METODO DE CADASTRAR
-@app.route('/cadastro', methods=['GET', 'POST'])
+@app.route('/produto/cadastro', methods=['GET', 'POST'])
 #METODO DE CADASTRAR
-def cadastro():
+def cadastroProdutos():
     if request.method == 'POST':
-        nome = request.form.get('nome')
-        telefone = request.form.get('telefone')
-        cpf = request.form.get('cpf')
-        email = request.form.get('email')
+        nomeProduto = request.form['nomeProduto']
+        valorProduto = request.form['valorProduto']
+        qntEstoque = request.form['qntEstoque']
 
-        if nome and telefone and cpf and email:
-            p = Pessoa(nome, telefone, cpf, email)
+        if nomeProduto and valorProduto and qntEstoque:
+            p = Produto(nomeProduto = nomeProduto, valorProduto = valorProduto, qntEstoque = qntEstoque)
             db.session.add(p)
             db.session.commit()
 
         return redirect(url_for('index'))
 
 #ROTA DA PAGINA DE LISTA
-@app.route('/lista')
+@app.route('/produtos')
 #METODO DE LISTAR
-def lista():
-    pessoas = Pessoa.query.all()
-    return render_template('lista.html', pessoas=pessoas)
+def listarProdutos():
+    produtos = Produto.query.all()
+    return render_template('listaProdutos.html', produtos=produtos)
 
 #excluir pelo ID
-@app.route('/excluir/<int:id>')
-def excluir(id):
-    pessoa = Pessoa.query.filter_by(_id=id).first()
-    db.session.delete(pessoa)
+@app.route('/produtos/excluir/<int:codProduto>')
+def excluirProdutos(codProduto):
+    produto = Produto.query.filter_by(_codProduto=codProduto).first()
+    db.session.delete(produto)
     db.session.commit()
     #apos excluir vai voltar para pagina de listar
-    pessoas = Pessoa.query.all()
-    return render_template('lista.html', pessoas=pessoas)
+    produtos = Produto.query.all()
+    return render_template('listaProdutos.html', produtos=produtos)
 
 #ATUALIZAR pelo ID recebendo um get do parametro e retornando um post
-@app.route('/atualizar/<int:id>', methods=['GET', 'POST'])
-def atualizar(id):
-    pessoa = Pessoa.query.filter_by(_id=id).first()
+@app.route('/produtos/atualizar/<int:codProduto>', methods=['GET', 'POST'])
+def atualizarProdutos(codProduto):
+    produto = Produto.query.filter_by(_codProduto=codProduto).first()
     
     if request.method == 'POST':
-        nome = request.form.get('nome')
-        telefone = request.form.get('telefone')
-        cpf = request.form.get('cpf')
-        email = request.form.get('email')
+        nomeP = request.form.get('nomeProduto')
+        valorP = request.form.get('valorProduto')
+        qntEstoqueP = request.form.get('qntEstoque')
 
-        if nome and telefone and cpf and email:
-            pessoa.nome = nome
-            pessoa.telefone = telefone
-            pessoa.cpf = cpf
-            pessoa.email = email
+        if nomeP and valorP and qntEstoqueP:
+            produto.nomeProduto = nomeP
+            produto.valorProduto = valorP
+            produto.qntEstoque = qntEstoqueP
 
             db.session.commit()
 
-            return redirect(url_for('lista'))
+            return redirect(url_for('listarProdutos'))
 
-    return render_template('atualizar.html',pessoa=pessoa)
+    return render_template('atualizarProdutos.html',produto=produto)
+
+
 
 #inicia o aplicativo
 if __name__ == '__main__':
