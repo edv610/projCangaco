d2e9db10bf3f8f006c00f80f87a0b5227720c492
edv610 <50935061+edv610@users.noreply.github.com>
2020-11-22 06:03:29 -0300

    Crud de Produtos Adicionado

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def cadastroUsuario():
     if request.method == 'POST':
         email = request.form.get('email')
         senha = request.form.get('senha')
-        adm = request.form.get('adm')
+        adm = 'Off' if request.form.get('adm') is None else 'On'
 
         if email and senha and adm:
             u = Usuario(email, senha, adm)
@@ -78,7 +78,7 @@ def atualizarUsuario(id):
     if request.method == 'POST':
         email = request.form.get('email')
         senha = request.form.get('senha')
-        adm = request.form.get('adm')
+        adm = 'Off' if request.form.get('adm') is None else 'On'
 
 
         if email and senha and adm:
@@ -91,7 +91,88 @@ def atualizarUsuario(id):
             return redirect(url_for('listaUsuario'))
 
     return render_template('atualizarUsuario.html',usuario=usuario)
+
 #----------------------------------------------------------------CLASSE PRODUTO--------------------------------------------------------------------
+
+class Produto(db.Model):
+    #CRIAÇÃO DA TABELA DO BANCO
+    __tablename='produto'
+    _codProduto = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nomeProduto = db.Column(db.String)
+    valorProduto = db.Column(db.Float)
+    qntEstoque = db.Column(db.Integer)
+
+    #CONSTRUTOR
+    def _init_(self,nomeProduto,valorProduto,qntEstoque):
+        self.nomeProduto = nomeProduto
+        self.valorProduto = valorProduto
+        self.qntEstoque = qntEstoque
+
+db.create_all()
+
+#ROTA DA PAGINA USUÁRIO
+@app.route('/produtoPage')
+def produtoPage():
+    return render_template('produto.html')
+
+#ROTA DA  PAGINA DO CADASTRO
+@app.route('/produto/cadastrar')
+def cadastrarProdutos():
+    return render_template('cadastroProdutos.html')
+
+#ROTA DO METODO DE CADASTRAR
+@app.route('/produto/cadastro', methods=['GET', 'POST'])
+#METODO DE CADASTRAR
+def cadastroProdutos():
+    if request.method == 'POST':
+        nomeProduto = request.form['nomeProduto']
+        valorProduto = request.form['valorProduto']
+        qntEstoque = request.form['qntEstoque']
+
+        if nomeProduto and valorProduto and qntEstoque:
+            p = Produto(nomeProduto = nomeProduto, valorProduto = valorProduto, qntEstoque = qntEstoque)
+            db.session.add(p)
+            db.session.commit()
+
+        return redirect(url_for('produtoPage'))
+
+#ROTA DA PAGINA DE LISTA
+@app.route('/produtos')
+#METODO DE LISTAR
+def listarProdutos():
+    produtos = Produto.query.all()
+    return render_template('listaProdutos.html', produtos=produtos)
+
+#excluir pelo ID
+@app.route('/produtos/excluir/<int:codProduto>')
+def excluirProdutos(codProduto):
+    produto = Produto.query.filter_by(_codProduto=codProduto).first()
+    db.session.delete(produto)
+    db.session.commit()
+    #apos excluir vai voltar para pagina de listar
+    produtos = Produto.query.all()
+    return render_template('listaProdutos.html', produtos=produtos)
+
+#ATUALIZAR pelo ID recebendo um get do parametro e retornando um post
+@app.route('/produtos/atualizar/<int:codProduto>', methods=['GET', 'POST'])
+def atualizarProdutos(codProduto):
+    produto = Produto.query.filter_by(_codProduto=codProduto).first()
+    
+    if request.method == 'POST':
+        nomeP = request.form.get('nomeProduto')
+        valorP = request.form.get('valorProduto')
+        qntEstoqueP = request.form.get('qntEstoque')
+
+        if nomeP and valorP and qntEstoqueP:
+            produto.nomeProduto = nomeP
+            produto.valorProduto = valorP
+            produto.qntEstoque = qntEstoqueP
+
+            db.session.commit()
+
+            return redirect(url_for('listarProdutos'))
+
+    return render_template('atualizarProdutos.html',produto=produto)
 
 
 
